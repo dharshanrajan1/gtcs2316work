fb04130c5ac0a1757c036b9a30dfab816f258fce
dharshanrajan1 <94082272+dharshanrajan1@users.noreply.github.com>
2024-05-09 15:37:13 -0400

    Delete tempy.py

--- tempy.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('kingdoms.csv', 'r') as fin: 
-    reader = csv.reader(fin)
-    readerlist = [line for line in reader]
-    
-    for i in readerlist[1:]:
-        print(tuple(i))
